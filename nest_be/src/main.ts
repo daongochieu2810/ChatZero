992e205ef612894d86bde74bb6219e44d993a7bb
Dao Ngoc Hieu <daongochieu2810@yahoo.com.vn>
2021-05-10 16:19:59 +0800

    Add microservice server

--- nest_be/src/main.ts
+++ nest_be/src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { SocketAdapter } from './sockets/socket.adapter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  app.useWebSocketAdapter(new SocketAdapter(app, true));
-  await app.listen(3001);
+  const mainApp = await NestFactory.create(AppModule, { cors: true });
+  const microServiceServer = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: 3002
+      }
+    }
+  )
+
+  mainApp.useWebSocketAdapter(new SocketAdapter(mainApp, true));
+  await mainApp.listen(3001, () => console.log('Http server is listening at port 3001'));
+  microServiceServer.listen(() => console.log('Microservice server is listening at port 3002'))
 }
 bootstrap();
